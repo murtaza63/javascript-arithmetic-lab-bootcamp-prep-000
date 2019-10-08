0ac09f99724b5a2a13ae99d358f325eabab9f008
breezy-constructor-6081 <murtaza63@gmail.com>
2019-10-08 10:06:22 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b){
+return a+b;
+}
+add(4,3);
+function subtract(a, b){
+  return a-b;
+}
+subtract(7,3);
+
+function multiply(a,b){
+return a*b;
+}
+multiply(4,3);
+function divide(a,b){
+return a/b;
+}
+divide(4,3);
+function inc(n){
+n++;
+return n;
+}
+inc(85);
+function dec(n){
+n--;
+return n;
+}
+inc(85);
+
+function maktInt(n){
+return parseInt(n)
+}
+inc('5');
+
